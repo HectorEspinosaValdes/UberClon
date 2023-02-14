2c6400022df2da47c30eb9c76445f8cd354a5074
luis <luis.hev1920@gmail.com>
2023-02-14 17:25:58 -0600

    login terminado

--- app/src/main/java/com/ubertron/MainActivity.kt
+++ app/src/main/java/com/ubertron/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ubertron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
